c6829934f52469c47d14d6e9055bd085484c571c
tushartrip1010 <72696630+tushartrip1010@users.noreply.github.com>
2021-08-29 18:00:31 +0530

    Add files via upload

---
+++ Day 2/armstrong_no.py
@@ -0,0 +1,11 @@
+def armstrong(a):
+    temp=a
+    result=0
+    order=len(str(a))
+    while temp>0:
+        digit=temp%10
+        result+=digit**order
+        temp//=10
+    return "It's an Armstrong Number" if (result == a) else "No, It's not an Armstrong Number"
+a = int(input("Enter a Number: "))
+print(f"{armstrong(a)}")
